98ce6d727e0333729cc7b826125d8d99badd8026
Harshit Sharma <1822sharma@gmail.com>
2022-04-11 18:17:07 +0530

    Function Program

--- Functions.sol
+++ Functions.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.5.11
 
 contract Mycontract {
+    uint value;
 
+    function getvalue() external view returns(uint){
+
+        return value;
+    }
+    function setvalue(uint _value) external {
+        value = _value;
+        
+    }
 }
